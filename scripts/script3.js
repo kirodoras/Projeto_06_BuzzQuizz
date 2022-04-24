f77dcc4480719e509dcf1c923d16592acebef08a
MateusFigueiredo <figueiredo1497@gmail.com>
2022-04-24 19:25:53 -0300

    Faltou o zero

--- scripts/script3.js
+++ scripts/script3.js
@@ -349,7 +349,7 @@ function temZero(arr){
 
 function verificaCor(str){
     let arr = [];
-    const hex = ['1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','a','b','c','d','e','f'];
+    const hex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','a','b','c','d','e','f'];
     for(let i = 0; i < str.length; i++){
         arr[i] = str[i];
     }
